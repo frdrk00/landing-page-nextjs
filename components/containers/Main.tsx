01bc9c72b6bd1654c799a5729b3d6d867f8e6d5d
frdrk00 <masterkooo@gmail.com>
2023-10-07 16:11:36 +0200

    Main - Updated

--- components/containers/Main.tsx
+++ components/containers/Main.tsx
@@ -6,6 +6,7 @@ const Main = () => {
     >
       <div className="flex flex-row mt-14">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">
+          {/* Title */}
           <div className="w-full h-full flex flex-col items-center lg:items-start justify-center">
             <h2 className="text-[87px] text-texlight font-semibold text-white leading-[92.5px] font-sans">
               Objavte neobmedzené{' '}
@@ -14,14 +15,17 @@ const Main = () => {
               </span>
             </h2>
 
-            <div className="text-[#C9D4F0] my-10">
-              <p>Sme IT spoločnosť, ktorá využíva pokročilé technológie.</p>
-              <p>
+            <div className="my-10">
+              <p className="text-gray-400 text-xl not-italic font-normal leading-7">
+                Sme IT spoločnosť, ktorá využíva pokročilé technológie.
+              </p>
+              <p className="text-gray-400 text-xl not-italic font-normal leading-7">
                 Našim poslaním je poskytnúť svojim klientom neobmedzené
                 <br /> možnosti v rámci IT segmentu.
               </p>
             </div>
 
+            {/* Buttons */}
             <div className="flex flex-row space-x-4 w-full h-auto">
               <div className="bg-gradient-to-r from-[#1E1741] to-[#5E16F9] p-[1px] rounded-full">
                 <button className="px-8 py-3 rounded-full h-auto w-full">
@@ -35,35 +39,33 @@ const Main = () => {
               </div>
             </div>
           </div>
-          <div className="w-full ">
-            <div>
-              <img
-                src="/rec.svg"
-                className="opacity-[40%] p-2 absolute z-10 object-contain"
-              />
-            </div>
-            <img
-              src="/blobanimation.svg"
-              className="absolute z-20"
-            />
+
+          {/* Blob */}
+          <div className="rounded-full border-t-1.5 border-r-1.5 border-b-1.5 bg-gradient-to-br from-opacity-21 via-opacity-21 to-opacity-0 shadow-md">
+            <img src="/blobanimation.svg" className="w-full h-full" />
           </div>
         </div>
       </div>
 
-        <div className="w-full flex flex-row space-x-10 px-4 my-10 text-white font-sans">
-          <div className="flex flex-col justify-center items-center ">
-            <h1 className="text-2xl font-semibold">97 +</h1>
-            <p className="text-xs text-center">
-              Spokojných
-              <br /> klientov
-            </p>
+      {/* Numbers */}
+      <div className="w-full flex flex-row space-x-12 px-4 my-4 text-white">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-5xl text-white text-center font-sans font-semibold leading-normal">
+            97 +
+          </h1>
+          <p className="text-center text-base font-medium">
+            Spokojných
+            <br /> klientov
+          </p>
         </div>
-          <div className="flex flex-col justify-center items-center ">
-            <h1 className="text-2xl font-semibold">5</h1>
-            <p className="text-xs text-center">
-              Vývojárksych
-              <br /> tímov
-            </p>
+        <div className="flex flex-col justify-center items-center ">
+          <h1 className="text-5xl text-white text-center font-sans font-semibold leading-normal">
+            5
+          </h1>
+          <p className="text-center text-base font-medium">
+            Vývojárkych
+            <br /> tímov
+          </p>
         </div>
       </div>
     </section>
